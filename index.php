1f3a69b79dbf31cc6bd33b280f85137ce2a82ba9
dvi- <contact@damienchiboub.fr>
2013-04-19 20:46:25 +0200

    first files

---
+++ index.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+<script src="http://code.jquery.com/jquery.min.js"></script>
+<link href="http://twitter.github.com/bootstrap/assets/css/bootstrap.css" rel="stylesheet" type="text/css" />
+<link href="http://twitter.github.com/bootstrap/assets/css/bootstrap-responsive.css" rel="stylesheet" type="text/css" />
+<script src="http://twitter.github.com/bootstrap/assets/js/bootstrap.js"></script>
+    <meta charset="utf-8">
+    <title>reverse-engineering.fr</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+  
+    <!-- Le styles -->
+    <style type="text/css">
+      body {
+        background-color:#FFFFFF;
+        padding-top: 20px;
+        padding-bottom: 40px;
+      }
+
+      /* Custom container */
+      .container-narrow {
+        margin: 0 auto;
+        max-width: 980px;
+      }
+      .container-narrow > hr {
+        margin: 30px 0;
+      }
+
+      /* Main marketing message and sign up button */
+      .jumbotron {
+        margin: 60px 0;
+        text-align: center;
+      }
+      .jumbotron h1 {
+        font-size: 42px;
+        line-height: 1;
+      }
+      .jumbotron .btn {
+        font-size: 21px;
+        padding: 14px 24px;
+      }
+
+      /* Supporting marketing content */
+      .marketing {
+        margin: 60px 0;
+      }
+      .marketing p + h4 {
+        margin-top: 28px;
+      }
+      
+      
+
+      
+    </style>
+    <link href="../assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="../assets/ico/favicon.png">
+  </head>
+
+  <body>
+
+    <div class="container-narrow">
+      
+        <div class="navbar navbar-inverse" style="position: static;">
+              <div class="navbar-inner">
+                <div class="container">
+                  <a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </a>
+                 <form class="navbar-search pull-left" action="">
+                      <input type="text" class="search-query span2" placeholder="Search">
+                    </form>
+                  
+                  
+                  
+                  
+                  
+                  <div class="nav-collapse collapse navbar-inverse-collapse">
+                    <ul class="nav">
+                      
+           <ul class="nav">
+                      <li class="divider-vertical"></li>
+                      <li class=""><a href="#">Home</a></li>                
+                      </ul>           
+<li><a href="#">Work</a></li>
+<li><a href="#">Spam</a></li>                      
+                 <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                          <li><a href="#">Action</a></li>
+                          <li><a href="#">Another action</a></li>
+                          <li><a href="#">Something else here</a></li>
+                          <li class="divider"></li>
+                          <li><a href="#">Separated link</a></li>
+                        </ul>
+                      </li>
+                      
+                      <li><a href="#">Contact</a></li>
+
+                    </ul>
+                    
+                    
+                  </div><!-- /.nav-collapse -->
+                </div>
+              </div><!-- /navbar-inner -->
+            </div>
+      <div class="row-fluid">
+      <div class="span8 offset2 jumbotron">
+        <h1>reverse-engineering.fr</h1>
+      
+         <p>will be back soon</p>
+  <p>
+    
+  </p>
+</div>
+</div>      
+      
+      
+      
+  
+ <div class="row-fluid">
+  <center> 
+  <ul class="breadcrumb">
+  <li class="active"><a href="#">Home</a> <span class="divider">/</span></li>
+  <li><a href="#">Library</a> <span class="divider">/</span></li>  
+        <li><a href="#">Size</a> <span class="divider">/</span></li>
+        <li><a href="#">Contact</a></li>
+    </center></ul>
+  
+   
+   
+   
+   <ul class="span2 nav nav-tabs nav-stacked">
+              <li><a class="accordion-toggle" data-toggle="collapse" href="#Menu1">Menu1<i class="icon-chevron-right pull-right"></i></a></li>
+              <li><a class="accordion-toggle" data-toggle="collapse" href="#Menu2">Menu2 <i class="icon-chevron-right pull-right"></i></a></li>
+              <li><a class="accordion-toggle" data-toggle="collapse" href="#Menu3">Menu3 <i class="icon-chevron-right pull-right"></i></a></li>
+            </ul>
+   
+  
+   
+  
+   <div class="span8 tabbable"> <!-- Only required for left/right tabs -->
+  <ul class="nav nav-tabs ">
+    <li class="active"><a href="#tab1" data-toggle="tab">Section 1</a></li>
+    <li><a href="#tab2" data-toggle="tab">Section 2</a></li>
+    <li><a href="#tab3" data-toggle="tab">Section 3</a></li>
+     </ul>
+     <div class="lead tab-content">
+    <div class="tab-pane active" id="tab1">
+      <p>Sleek, intuitive, and powerful front-end framework for faster and easier web development.</p>
+    </div>
+    <div class="tab-pane" id="tab2">
+      <p>Howdy, I'm in Section 2.</p>
+    </div>
+       <div class="tab-pane" id="tab3">
+      <p>Howdy, I'm in Section 3.</p>
+    </div>
+    </div>
+  </div>
+</div>
+     
+ 
+     
+     
+  </div>
+   </div>
+   
+   
+   
+      </body>
+    </html>
---
+++ old.index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome on reverse-engineering.fr Webapp</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="reverse-engineering.fr, vulns, disclosures, dataleaks, and others news services">
+    <meta name="author" content="dvi-">
+    <meta name="robots" content="index,follow,archive">
+  
+    <!-- Stylesheet -->
+    <link href="/assets/css/bootstrap.css" rel="stylesheet">
+    <link href="/assets/css/adds.css" rel="stylesheet">
+    <link href="/assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container-narrow">
+
+      <div class="masthead">
+        <ul class="nav nav-pills pull-right">
+          <li class="active"><a href="http://www.reverse-engineering.fr">Home</a></li>
+        </ul>
+        <h3 class="muted">reverse-engineering.fr <small class="hidden-tablet hidden-phone"> <?php print  date('{ H:i:s - m/d/Y }');?> </h3></small>
+      </div>
+
+      <hr>
+
+      <div class="jumbotron">
+        <h1>Another Security Contribution</h1><p>
+	<p class="lead">After near one year of work, the first version of <a href="http://www.reverse-engineering.fr">reverse-engineering.fr</a><br>
+	 that discovered more than one hundred security
+	vulns, is actually rebuilt and cleaned to deliver a better ethical contribution.</p>
+	<div class="progress">
+        <div class="bar bar-success" style="width: 10%;"></div>
+        <div class="bar bar-danger" style="width: 2%;"></div>
+        <div class="bar bar-warning" style="width: 88%;"></div>
+
+        </div>
+
+
+	<button href="#Update" role="button" data-toggle="modal">Come back later</button>
+ 	<!-- Modal -->
+	<div id="Update" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="Update" aria-hidden="true">
+  	<div class="modal-header">
+    	<h3 id="Update">Come back later</h3>
+  	</div>
+  	<div class="modal-body">
+    	<p>Actually working on php poo mvc & orm<br>
+	project may be back in the first part of 2013.</p>
+  	</div>
+  	<div class="modal-footer">
+    	<button class="btn" data-dismiss="modal" aria-hidden="true">x</button>
+ 	 </div>
+	</div>
+	</div>
+	<hr>
+      <div class="row-fluid marketing">
+        <div class="span6">
+	<h4>New Project</h4>
+	<p class="text-info">Come back in version 2, made with a growing php framework.</p>
+	<h4>Responsive Design  <a href="#" onClick="window.open('http://www.reverse-engineering.fr', 'Check', 'scrollbars=yes,width=400,height=500,top=120,left=480')"><span class="badge">Check</span></h4></a>
+        <p class="text-info">Dynamic and interactive content, Multi-devices.</p>
+
+	<h4>Professional Hosting</h4>
+	<p class="text-info"> Hosted in a dedicated server, adjustable power.</p>
+        </div>
+
+        <div class="span6">
+        <h4>Visibility</h4>
+	<p class="text-info">Better visibility, App works faster, dynamic content, natural indexation.</p>
+
+	<h4>Reactions</h4>
+	<p class="text-success">Must register to post comments or use news services.</p>
+
+	<h4>Services</h4>
+	<p class="text-error">Exploits, Vulns, Responsible Disclosure, Warning Process, Hackers jobs publication.</p>
+	</div>
+      </div>
+
+      <hr><br>
+      <div class="footer">
+      <a href="http://www.reverse-engineering.fr">  <button class="btn btn-large btn-block" type="button"><p class="text-info">reverse-engineering.fr</button></p></a>
+      </div>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="/assets/js/jquery.js"></script>
+    <script src="/assets/js/bootstrap-transition.js"></script>
+    <script src="/assets/js/bootstrap-alert.js"></script>
+    <script src="/assets/js/bootstrap-modal.js"></script>
+    <script src="/assets/js/bootstrap-dropdown.js"></script>
+    <script src="/assets/js/bootstrap-scrollspy.js"></script>
+    <script src="/assets/js/bootstrap-tab.js"></script>
+    <script src="/assets/js/bootstrap-tooltip.js"></script>
+    <script src="/assets/js/bootstrap-popover.js"></script>
+    <script src="/assets/js/bootstrap-button.js"></script>
+    <script src="/assets/js/bootstrap-collapse.js"></script>
+    <script src="/assets/js/bootstrap-carousel.js"></script>
+    <script src="/assets/js/bootstrap-typeahead.js"></script>
+
+  </body>
+</html>
